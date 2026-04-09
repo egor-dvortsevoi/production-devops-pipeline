f8b6124321dd3195775f6ceaa67a6973304a92fa
Felix <felix.mw.gt@gmail.com>
2026-04-09 14:07:41 +0300

    test1

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+terraform{
+    required_providers {
+      kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = "~> 2.0"
+      }
+    }
+}
+
+# resource "kubernetes_deployment" "nginx" {
+#   metadata {
+#     name = "nginx-deployment"
+#     labels = {
+#       app = "nginx"
+#     }
+#   }
+
+#   spec {
+#     replicas = 1
+
+#     selector {
+#       match_labels = {
+#         app = "nginx"
+#       }
+#     }
+
+#     template {
+#       metadata {
+#         labels = {
+#           app = "nginx"
+#         }
+#       }
+
+#       spec {
+#         container {
+#           name  = "nginx"
+#           image = "nginx:latest"
+
+#           port {
+#             container_port = 80
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+
+# resource "kubernetes_service" "nginx" {
+#   metadata {
+#     name = "nginx-service"
+#   }
+
+#   spec {
+#     selector = {
+#       app = "nginx"
+#     }
+
+#     port {
+#       port        = 80
+#       target_port = 80
+#       node_port   = 30007
+#     }
+
+#     type = "NodePort"
+#   }
+# }
